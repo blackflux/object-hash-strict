70813cf97c9ce5dda3bb09db893bbd79cf04f376
Lukas Siemon <lukas.siemon@gmail.com>
2019-08-19 22:48:56 -0700

    feat: added functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-module.exports = (a, b) => a + b;
+const objectHash = require('object-hash');
+const objectScan = require('object-scan');
+
+const INVALID = [undefined];
+const scanner = objectScan(['**'], {
+  filterFn: (key, value) => {
+    if (INVALID.includes(value)) {
+      throw new Error(`Bad value "${value}" for key "${key}" detected`);
+    }
+  }
+});
+
+const validateObject = (obj) => {
+  scanner(obj);
+  return obj;
+};
+
+const wrap = (fn) => (object, ...args) => fn(validateObject(object), ...args);
+
+module.exports = Object.entries(objectHash).reduce((p, [k, v]) => {
+  // eslint-disable-next-line no-param-reassign
+  p[k] = wrap(v);
+  return p;
+}, wrap(objectHash));
