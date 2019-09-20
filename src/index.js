e22e609034369b983f3e792e25bf8625fd7895fa
Lukas Siemon <github@blackflux.com>
2019-09-20 10:11:04 -0700

    fix: removed lodash.get dependency

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const assert = require('assert');
-const get = require('lodash.get');
 const objectHash = require('object-hash');
 const objectScan = require('object-scan');
 
 const INVALID = [undefined];
-const scanner = (obj, allowedUndefined) => objectScan(['**'], {
+const scanner = (obj, { allowedUndefined }) => objectScan(['**'], {
   filterFn: (key, value) => {
     if (INVALID.includes(value) && !allowedUndefined.includes(key)) {
       throw new Error(`Bad value "${value}" for key "${key}" detected`);
@@ -12,13 +11,13 @@ const scanner = (obj, allowedUndefined) => objectScan(['**'], {
   }
 })(obj);
 
-const validateObject = (obj, allowedUndefined) => {
+const validateObject = (obj, { allowedUndefined = [] } = {}) => {
   assert(Array.isArray(allowedUndefined), 'Invalid Option Provided');
-  scanner(obj, allowedUndefined);
+  scanner(obj, { allowedUndefined });
   return obj;
 };
 
-const wrap = (fn) => (object, ...args) => fn(validateObject(object, get(args[0], 'allowedUndefined', [])), ...args);
+const wrap = (fn) => (object, ...args) => fn(validateObject(object, args[0]), ...args);
 
 module.exports = Object
   .entries(objectHash)
